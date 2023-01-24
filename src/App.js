3a32d2fb005ba5ba2f936c68896ea29a568d8470
MoreiraSte <casagrandestephany91@gmail.com>
2023-01-24 16:28:31 -0300

    test library - success

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import VLibras from "@djpfs/react-vlibras";
+
+
+
+// import VLibras from './index'
 
 function App() {
   return (
     <div className="App">
+      <VLibras forceOnload={true} />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Teste</h1>
+
+        <p>Acessibilidade vlibras</p>
+
+        <h3>Vlibras</h3>
+
+        <p>traduzir</p>
+
+        
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
